87ce9a9f4b1f6af281ed16cd86d4412f9c5248e1
TEJASH2007 <ktreijsahsnha2007@gmail.com>
2026-07-13 15:22:03 +0530

    n = {0,1} cod=ndition checked

--- LAB2/Q1.js
+++ LAB2/Q1.js
@@ -1,10 +1,11 @@
 let n = prompt("Number : ");
 let flag =  true;
+
 for(let i=2;i*i<=n;i++){
     if(n%i==0){
         flag = false;
         break;
     }
 }
-console.log(flag);
+console.log((flag && n>=2)?("Number is Prime "):("Number is not Prime"));
 
